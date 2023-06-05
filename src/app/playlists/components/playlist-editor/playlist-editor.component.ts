465dcfed9a9c248a662e7ef9bc4242683aa8b869
Mateusz Kulesza <ev45ive@gmail.com>
2023-06-05 13:39:48 +0200

    Exercise: Fill in form controls with mock data

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
-  styleUrls: ['./playlist-editor.component.scss']
+  styleUrls: ['./playlist-editor.component.scss'],
 })
 export class PlaylistEditorComponent {
-
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Best playlist',
+  };
 }
