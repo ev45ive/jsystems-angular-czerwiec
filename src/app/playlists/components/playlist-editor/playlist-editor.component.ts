5315dbbb7dd0d5a2bb70cd8f3aff96a267e3dee9
Mateusz Kulesza <ev45ive@gmail.com>
2023-06-05 14:13:12 +0200

    Ingore outside events // ChangeDetectionStrategy.OnPush

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrls: ['./playlist-editor.component.scss'],
+
+  // Ingore outside events
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlaylistEditorComponent {
   playlist = {
